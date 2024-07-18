57c990c95fb7bcab1a39590f53660e785aec4394
newclea <114222079+newclea@users.noreply.github.com>
2024-07-18 12:31:41 +0800

    feat: TestFrame

--- include/opt/DataflowAnalysis.hh
+++ include/opt/DataflowAnalysis.hh
@@ -9,6 +9,20 @@
 template <typename Fact>
 class DataflowAnalysis {
  public:
+    struct {
+        void (*teardown) (DataflowAnalysis *t);
+        bool (*isForward) (DataflowAnalysis *t);
+        Fact *(*newBoundaryFact) (DataflowAnalysis *t, Function *func);
+        Fact *(*newInitialFact) (DataflowAnalysis *t);
+        void (*setInFact) (DataflowAnalysis *t, BasicBlock *blk, Fact *fact);
+        void (*setOutFact) (DataflowAnalysis *t, BasicBlock *blk, Fact *fact);
+        Fact *(*getInFact) (DataflowAnalysis *t, BasicBlock *blk);
+        Fact *(*getOutFact) (DataflowAnalysis *t, BasicBlock *blk);
+        bool (*meetInto) (DataflowAnalysis *t, Fact *fact, Fact *target);
+        bool (*transferBlock) (DataflowAnalysis *t, BasicBlock *block, Fact *in_fact, Fact *out_fact);
+        void (*printResult) (DataflowAnalysis *t, BasicBlock *func);
+    } const *vTable;
+
   // return true if analysis is forward
   virtual bool isForward() = 0;
 
@@ -25,5 +39,8 @@ class DataflowAnalysis {
   // transfer function
   virtual bool transferBlock(BasicBlock* bb, Fact in, Fact out);
 };
+
+
+
 
 #endif
