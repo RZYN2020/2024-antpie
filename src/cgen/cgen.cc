a38c83a53f45af8cd7f9cc36c747185f2a5558a4
zyz <zhaoyzzz@outlook.com>
2024-07-29 14:46:47 +0800

    try allocate all

--- src/cgen/cgen.cc
+++ src/cgen/cgen.cc
@@ -9,11 +9,11 @@ void generate_code(MModule *res, ANTPIE::Module *ir, bool opt) {
   // std::ofstream out_s;
   // out_s.open("tests/test.phi.s");
   // out_s << *res;
-  if (opt) {
-    allocate_register(res);
-  } else {
-    spill_all_register(res);
-  }
+  // if (opt) {
+  allocate_register(res);
+  // } else {
+  // spill_all_register(res);
+  // }
 
   peephole_optimize(res);
 
