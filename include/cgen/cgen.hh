c5555a2cf272c6e8164e57a5351da4763a7bd4e9
zyz <zhaoyzzz@outlook.com>
2024-08-15 21:46:12 +0800

    init scheduling

--- include/cgen/cgen.hh
+++ include/cgen/cgen.hh
@@ -8,7 +8,7 @@ void generate_code(MModule* res, ANTPIE::Module* ir, bool opt);
 void select_instruction(MModule* res, ANTPIE::Module* ir);
 void ssa_liveness_analysis(MFunction *func, LivenessInfo *liveness_i,
                            LivenessInfo *liveness_f);
-void lower_register_pressure(MModule* res);
+void schedule_instruction(MModule *mod);
 void allocate_register(MModule *mod);
 void spill_all_register(MModule* mod);
 void peephole_optimize(MModule* mod);
