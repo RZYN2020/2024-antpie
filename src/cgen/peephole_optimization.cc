807b10abafae4d7ffc6e0ef2ebc20125e8a4a56f
zyz <zhaoyzzz@outlook.com>
2024-08-16 22:18:23 +0800

    fix rm redundent store load bug

--- src/cgen/peephole_optimization.cc
+++ src/cgen/peephole_optimization.cc
@@ -11,20 +11,20 @@ bool is_store(MInstruction *ins) {
 }
 int get_store_imm(MInstruction *store) {
   switch (store->getInsTag()) {
-    case MInstruction::SW: {
-      auto sw = static_cast<MIsw *>(store);
-      return sw->imm;
-    }
-    case MInstruction::SD: {
-      auto sd = static_cast<MIsd *>(store);
-      return sd->imm;
-    }
-    case MInstruction::FSW: {
-      auto fsw = static_cast<MIfsw *>(store);
-      return fsw->imm;
-    }
-    default:
-      assert(0);
+  case MInstruction::SW: {
+    auto sw = static_cast<MIsw *>(store);
+    return sw->imm;
+  }
+  case MInstruction::SD: {
+    auto sd = static_cast<MIsd *>(store);
+    return sd->imm;
+  }
+  case MInstruction::FSW: {
+    auto fsw = static_cast<MIfsw *>(store);
+    return fsw->imm;
+  }
+  default:
+    assert(0);
   }
 }
 bool is_load(MInstruction *ins) {
@@ -34,32 +34,32 @@ bool is_load(MInstruction *ins) {
 }
 int get_load_imm(MInstruction *load) {
   switch (load->getInsTag()) {
-    case MInstruction::LW: {
-      auto lw = static_cast<MIlw *>(load);
-      return lw->imm;
-    }
-    case MInstruction::LD: {
-      auto ld = static_cast<MIld *>(load);
-      return ld->imm;
-    }
-    case MInstruction::FLW: {
-      auto lfw = static_cast<MIflw *>(load);
-      return lfw->imm;
-    }
-    default:
-      assert(0);
+  case MInstruction::LW: {
+    auto lw = static_cast<MIlw *>(load);
+    return lw->imm;
+  }
+  case MInstruction::LD: {
+    auto ld = static_cast<MIld *>(load);
+    return ld->imm;
+  }
+  case MInstruction::FLW: {
+    auto lfw = static_cast<MIflw *>(load);
+    return lfw->imm;
+  }
+  default:
+    assert(0);
   }
 }
 
 void scan_store_loads(MBasicBlock *bb, vector<vector<MInstruction *>> &ldsts) {
   int stk_off = 0;
 
-  unordered_map<Register *, bool> hold_val;  // if reg also hold the value?
+  unordered_map<Register *, bool> hold_val; // if reg also hold the value?
   unordered_map<Register *, int> reg_addr;
   unordered_map<int, Register *> addr_reg;
   // invarient: addr always holds the value
   map<Register *, vector<MInstruction *>>
-      tldsts;  // a list of load/stores which its value never changes
+      tldsts; // a list of load/stores which its value never changes
 
   for (auto ins : bb->getInstructions()) {
     if (ins->getInsTag() == MInstruction::ADDI &&
@@ -68,7 +68,7 @@ void scan_store_loads(MBasicBlock *bb, vector<vector<MInstruction *>> &ldsts) {
       stk_off += imm;
       continue;
     }
-    if (stk_off == 0) {  // not in call sequence
+    if (stk_off == 0) { // not in call sequence
 
       if (is_store(ins) && ins->getReg(1) == Register::reg_sp) {
         int off = get_store_imm(ins);
@@ -78,7 +78,7 @@ void scan_store_loads(MBasicBlock *bb, vector<vector<MInstruction *>> &ldsts) {
         }
         int addr = stk_off + off;
 
-        if (reg_addr.count(reg) > 0) {
+        if (reg_addr.count(reg) > 0 && reg_addr[reg] != addr) {
           ldsts.push_back(tldsts[reg]);
           tldsts[reg].clear();
           addr_reg.erase(reg_addr[reg]);
@@ -86,7 +86,7 @@ void scan_store_loads(MBasicBlock *bb, vector<vector<MInstruction *>> &ldsts) {
         }
         if (addr_reg.count(addr) > 0) {
           if (addr_reg[addr] == reg) {
-            if (hold_val[reg]) {  // find a redudent store
+            if (hold_val[reg]) { // find a redudent store
               // std::cout << endl << " Redudent store: " << *ins << endl;
               tldsts[reg].push_back(ins);
               // std::cout <<reg->getName() << " :"  << tldsts[reg].size() <<
@@ -105,7 +105,7 @@ void scan_store_loads(MBasicBlock *bb, vector<vector<MInstruction *>> &ldsts) {
           // std::cout << reg_addr.count(reg) << endl;
 
           tldsts[reg].push_back(ins);
-          hold_val[reg] = false;  // only load will be condsidered hold value
+          hold_val[reg] = false; // only load will be condsidered hold value
           reg_addr[reg] = addr;
           addr_reg[addr] = reg;
           // std::cout <<reg->getName() << " :"  << tldsts[reg].size() << endl;
@@ -146,18 +146,33 @@ void scan_store_loads(MBasicBlock *bb, vector<vector<MInstruction *>> &ldsts) {
       }
     }
 
+    static set<Register *> caller_saved_registers = {
+        Register::reg_t0,   Register::reg_t1,   Register::reg_t2,
+        Register::reg_a0,   Register::reg_a1,   Register::reg_a2,
+        Register::reg_a3,   Register::reg_a4,   Register::reg_a5,
+        Register::reg_a6,   Register::reg_a7,   Register::reg_t3,
+        Register::reg_t4,   Register::reg_t5,   Register::reg_t6,
+        Register::reg_ft0,  Register::reg_ft1,  Register::reg_ft2,
+        Register::reg_ft3,  Register::reg_ft4,  Register::reg_ft5,
+        Register::reg_ft6,  Register::reg_ft7,  Register::reg_fa0,
+        Register::reg_fa1,  Register::reg_fa2,  Register::reg_fa3,
+        Register::reg_fa4,  Register::reg_fa5,  Register::reg_fa6,
+        Register::reg_fa7,  Register::reg_ft8,  Register::reg_ft9,
+        Register::reg_ft10, Register::reg_ft11,
+    };
     if (ins->getInsTag() == MInstruction::CALL) {
-      // TODO: invalidate all caller saved registers...
-      if (hold_val.count(Register::reg_a0) > 0) {
-        hold_val[Register::reg_a0] = false;
+      for (auto reg : caller_saved_registers) {
+        if (hold_val.count(reg) > 0) {
+          hold_val[reg] = false;
+        }
       }
     }
 
     auto iuses = getUses<Register::I_REGISTER>(ins);
     for (auto reg : iuses) {
-      if (reg_addr.count(reg) > 0) {  // the reg is in stack
-        if (hold_val[reg]) {  // the value was used, then the last load can not
-                              // be deleted
+      if (reg_addr.count(reg) > 0) { // the reg is in stack
+        if (hold_val[reg]) { // the value was used, then the last load can not
+                             // be deleted
           // std::cout << endl << " Invalidate: " << *ins << endl;
           ldsts.push_back(tldsts[reg]);
           tldsts[reg].clear();
@@ -257,7 +272,8 @@ void peephole_optimize(MModule *mod) {
 
         // addi a0, a0, 0 # addi arr.addr.3, arr.addr.2, 0
         if (ins->getInsTag() == MInstruction::ADDI) {
-          if (ins->getTarget() == ins->getReg(0) && static_cast<MIaddi*>(ins)->imm == 0) {
+          if (ins->getTarget() == ins->getReg(0) &&
+              static_cast<MIaddi *>(ins)->imm == 0) {
             ins->replaceWith({});
             i++;
             // std::cout << "match " << " addi a0, a0, 0" << endl;
